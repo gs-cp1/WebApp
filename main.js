bba4627df889ac84a138ea28a2acbb57a85d3938
gs-cp1 <96265613+gs-cp1@users.noreply.github.com>
2024-08-14 16:38:05 -0400

    Update main.js

--- main.js
+++ main.js
@@ -5,4 +5,3 @@ document.addEventListener('DOMContentLoaded', () => {
    document.body.appendChild(message);
 });
 
-//creates a variable initialized as a constant. goes into html document using document command and then with the .createelemnt thing  creates whats in the parewntheseis hten next line then takes the message var and uses the .textconetent thing to add a messsage to the p elemetn that wasw just creates 
