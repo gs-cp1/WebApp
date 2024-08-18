97885ee271d0d10d4887ea2c2da5653f26fd5758
gs-cp1 <96265613+gs-cp1@users.noreply.github.com>
2024-08-18 13:00:54 -0400

    update and test

--- main.js
+++ main.js
@@ -3,5 +3,6 @@ document.addEventListener('DOMContentLoaded', () => {
    const message = document.createElement('p');
    message.textContent = 'This message is added by regular JavaScript!';
    document.body.appendChild(message);
+   
 });
 
